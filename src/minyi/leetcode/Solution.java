149b61b4c0984bb02ffd2a4de00f210b90fcfb41
Ding,Minyi <dingminyi@foxmail.com>
2016-07-21 00:24:23 +0800

    201607210024

--- src/minyi/leetcode/Solution.java
+++ src/minyi/leetcode/Solution.java
@@ -632,8 +632,32 @@ public class Solution {
         }
     }
     /////////////////////////////////////////////
-
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int origin = x;
+        int sum = 0;
+        while(x != 0){
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return sum == origin;
+    }
     /////////////////////////////////////////////
+    public boolean isPowerOfThree(int n) {
+        if(n<=0) return false;
+        while(n%3 == 0){
+            n = n/3;
+        }
+        if(n == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean isPowerOfThree2(int n) {
+        int maxPowerOfThree = (int)Math.pow(3, (int)(Math.log(Integer.MAX_VALUE) / Math.log(3)));
+        return n>0 && maxPowerOfThree%n==0;
+    }
     /////////////////////////////////////////////
     /////////////////////////////////////////////
     /////////////////////////////////////////////
