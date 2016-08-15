e49fd33304fba6b5e2d4c9711ed8a31652ea707d
Ding,Minyi <dingminyi@foxmail.com.com>
2016-08-15 11:55:53 +0800

    201608151155

--- src/minyi/leetcode/Solution.java
+++ src/minyi/leetcode/Solution.java
@@ -1003,7 +1003,49 @@ public class Solution {
         return lengthLastWord;
     }
     /////////////////////////////////////////////
+    public boolean canConstruct1(String ransomNote, String magazine) {
+        if( ransomNote.length() == 0 ){
+            return true;
+        }
+        for(int i = 0; i < magazine.length() - ransomNote.length() + 1; i++){
+            for(int j = 0; j < ransomNote.length();j++){
+                if(ransomNote.charAt(j) != magazine.charAt(i+j)){
+                    break;
+                }
+                if (j == ransomNote.length() - 1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     /////////////////////////////////////////////
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if( ransomNote.length() == 0 ){
+            return true;
+        }
+        HashMap<Character,Integer> mMap = new HashMap<>();
+        for(int i = 0; i < magazine.length(); i++){
+            char c = magazine.charAt(i);
+            if(mMap.containsKey(c)){
+                mMap.put(c, mMap.get(c) + 1);
+            }else{
+                mMap.put(c, 1);
+            }
+        }
+        for(int i = 0; i < ransomNote.length(); i++){
+            char c = ransomNote.charAt(i);
+            if(mMap.containsKey(c)){
+                mMap.put(c, mMap.get(c) - 1);
+                if(mMap.get(c) == 0){
+                    mMap.remove(c);
+                }
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
     /////////////////////////////////////////////
     /////////////////////////////////////////////
     /////////////////////////////////////////////
